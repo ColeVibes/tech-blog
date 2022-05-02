a431b9ce0605e3da5abba0383ff824c6343d28a0
J.Cole <ccjohn015@gmail.com>
2022-05-02 15:44:14 -0500

    added in home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,196 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../models/index')
 
+
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models/index');
+
+// View homepage with all the posts 
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true })); // Sequelize's get() method
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// View Login page, if user is already logged in, then page redirects to the homepage
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login')
+});
+
+// View Sign-up page
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+// View user's dashboard with all their posts 
+router.get('/dashboard', (req, res) => {
+
+    // reroute user to home page if they are not logged in 
+    if (!req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    };
+
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true })); // Sequelize's get() method
+            console.log(posts)
+            res.render('dashboard', {
+                posts,
+                username: posts[0],
+                loggedIn: req.session.loggedIn
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+// View single post page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn // if you don't have this the site will log you out
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// View single post page and edit or delete
+router.get('/editpost/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn // if you don't have this the site will log you out
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
